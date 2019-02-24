289bd93ccf2bd6f60fa03473f8bc89c26363eb30
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-02-24 19:30:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,14 +11,15 @@ class TicTacToe
     @board
   end
 
-  def WIN_COMBINATIONS
-      [0, 1, 2], #top row win
-      [3, 4, 5], #middle row win
-      [6, 7, 8], #bottom row win
-      [0, 4, 8], #diagonal win
-      [2, 4, 6], #diagonal win
-      [0, 3, 6], #left column win
-      [1, 4, 7], #middle column win
-      [2, 5, 8]  #right column win
-  end
+  WIN_COMBINATIONS = [
+  [0, 1, 2], #top row win
+  [3, 4, 5], #middle row win
+  [6, 7, 8], #bottom row win
+  [0, 4, 8], #diagonal win
+  [2, 4, 6], #diagonal win
+  [0, 3, 6], #left column win
+  [1, 4, 7], #middle column win
+  [2, 5, 8]  #right column win
+]
+
 end
