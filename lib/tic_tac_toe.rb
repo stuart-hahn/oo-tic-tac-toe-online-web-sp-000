1087e9d58026f802c97c99a5cdc0eb8872791ef5
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-02-24 19:33:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,7 +22,7 @@ class TicTacToe
   [2, 5, 8]  #right column win
 ]
 
-  def display_board(board)
+  def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
