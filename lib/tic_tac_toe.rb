5907d8679e1a1ceccc9e2d64e85967db034c7cfb
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-02-24 20:07:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,7 +79,7 @@ class TicTacToe
     display_board
   end
 
-  def won?(board)
+  def won?
     WIN_COMBINATIONS.detect do |win_combination|
       @board[win_combination[0]] == @board[win_combination[1]] && @board[win_combination[1]] == @board[win_combination[2]] && position_taken?(win_combination[0])
     end
