4f80018edb28c8459b794d6d6b98f2fec978ca2c
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-02-24 19:26:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,6 @@
 class TicTacToe
   def initialize
-    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
 
   def board=(board)
